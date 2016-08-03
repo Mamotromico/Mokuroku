730ee52d9cf94be7f22c226819ce7b0bcae67a30
mamotromico <mamotromico@gmail.com>
2016-08-03 17:07:29 -0300

    added barebones structure to the mangafox plugin

--- plugins/mangafox.js
+++ plugins/mangafox.js
@@ -1,3 +1,30 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var URL = require('url-parse');
+var request     = require('request');
+var cheerio     = require('cheerio');
+var URL         = require('url-parse');
+var fs          = require('fs');
+
+//Get every manga available on the site. Not sure if it should be used/needed
+function getCompleteMangaList () {
+
+}
+
+//Get manga page. Use name as a search term? not sure yet
+function getManga () {
+
+}
+
+//Get every chapter available from the manga
+function getChapterList () {
+
+}
+
+//Get manga metada available.
+//Ideally it should crossreference mangaupdates for more complete metadata
+function getMangaData () {
+
+}
+
+//Download every image on the chapter and save to system
+function downloadChapter () {
+
+}
