040d9f80377bb7177691d6154aae8986f0298587
mamotromico <mamotromico@gmail.com>
2016-08-04 16:21:20 -0300

    Getting all chapter links from a page done

--- plugins/mangafox.js
+++ plugins/mangafox.js
@@ -14,8 +14,29 @@ function getManga () {
 }
 
 //Get every chapter available from the manga
-function getChapterList () {
-
+function getChapterList (url) {
+  request(url, function (error, response, body) {
+    if (response.statusCode !== 200) {
+      console.log("Failed to load page, code: " + response.statusCode);
+      return;
+    }
+    var $ = cheerio.load(body);
+    var linkList = new Set();
+    //linkList.add($('.chlist > a').attr('href'));
+    console.log("aight, look for a");
+    $('.chlist a').each(function() {
+      console.log(this.attribs.href);
+      linkList.add(this.attribs.href);
+    });
+    for (let item of linkList) {
+      console.log(item);
+      var a = document.createElement('a');
+      a.href = item;
+      a.innerHTML = item;
+      document.body.appendChild(a);
+      document.body.appendChild(document.createElement('br'));
+    }
+  });
 }
 
 //Get manga metada available.
