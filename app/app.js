48b079be333332d0613a2c704668d282c4f88dca
mamotromico <mamotromico@gmail.com>
2016-08-08 01:12:12 -0300

    Main application code, will move the components to their own files

--- app/app.js
+++ app/app.js
@@ -4,5 +4,50 @@ var MANGAFOX = require('./../plugins/mangafox.js');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-console.log('oi');
-MANGAFOX.getCompleteMangaList();
+var mangafoxList = [];
+
+var MangaItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired
+  },
+  onClick: function(syntheticEvent) {
+    console.log(this.props.url);
+  },
+  render: function() {
+    return (
+      React.createElement('li', {},
+        // React.createElement('div', {}, this.props.name)
+        React.createElement('button', {type: "button", onClick: this.onClick}, this.props.name)
+      )
+    );
+  }
+});
+
+var rootElement =
+  React.createElement('div', {className: ""},
+    React.createElement('button', {type:  "button", onClick : updateMangafox}, "get list")
+  );
+
+ReactDOM.render(
+  rootElement,
+  document.getElementById('appRoot')
+);
+
+function updateMangafox () {
+  MANGAFOX.readCompleteMangaList(false, function(parsedJson) {
+    mangafoxList = parsedJson;
+    var mangafoxItemElements = mangafoxList.map(function(mangafoxItemElement) {
+      return React.createElement(MangaItem, mangafoxItemElement);
+    });
+    var newRootElement =
+      React.createElement('div', {className: ""},
+        React.createElement('button', {type:  "button", onClick : updateMangafox}, "get list"),
+        React.createElement('ul', {}, mangafoxItemElements)
+      );
+    ReactDOM.render(
+      newRootElement,
+      document.getElementById('appRoot')
+    );
+  });
+}
