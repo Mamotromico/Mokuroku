2b53130efde337e8be9d2b3e666942771860d10d
mamotromico <mamotromico@gmail.com>
2016-08-23 11:51:05 -0300

    New container will manage the display of chapter info

---
+++ app/containers/DownloaderWrapContainer.container.js
@@ -0,0 +1,17 @@
+require('./../components/DownloaderWrap.component.js');
+
+DownloaderWrapContainer = React.createClass({
+  getInitialState: function() {
+    return {
+
+    };
+  },
+  changeChapterList: function() {
+    
+  },
+  render:function() {
+    return React.createElement(DownloaderWrap);
+  }
+});
+
+module.exports = DownloaderWrapContainer;
